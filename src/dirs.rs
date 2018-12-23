2a7a6b373acee44bfbaad8848568e38b587863ee
Erik Bjäreholt <erik@bjareho.lt>
2018-12-23 20:12:56 +0100

    made use of appdirs platform-conditional to allow compilation under the Android

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,21 +1,48 @@
+#[cfg(not(target_os="android"))]
 use appdirs;
 
 use std::path::PathBuf;
 
+
 pub fn get_config_dir() -> Result<PathBuf, ()> {
-    let mut dir = appdirs::user_config_dir(Some("activitywatch"), None, false)?;
-    dir.push("aw-server-rust");
-    return Ok(dir);
+    #[cfg(not(target_os="android"))]
+    {
+        let mut dir;
+        dir = appdirs::user_config_dir(Some("activitywatch"), None, false)?;
+        dir.push("aw-server-rust");
+        return Ok(dir);
+    }
+
+    #[cfg(target_os="android")]
+    {
+        return Err(());
+    }
 }
 
 pub fn get_data_dir() -> Result<PathBuf, ()> {
-    let mut dir = appdirs::user_data_dir(Some("activitywatch"), None, false)?;
-    dir.push("aw-server-rust");
-    return Ok(dir);
+    #[cfg(not(target_os="android"))]
+    {
+        let mut dir = appdirs::user_data_dir(Some("activitywatch"), None, false)?;
+        dir.push("aw-server-rust");
+        return Ok(dir);
+    }
+
+    #[cfg(target_os="android")]
+    {
+        return Err(());
+    }
 }
 
 pub fn get_cache_dir() -> Result<PathBuf, ()> {
-    let mut dir = appdirs::user_cache_dir(Some("activitywatch"), None)?;
-    dir.push("aw-server-rust");
-    return Ok(dir);
+    #[cfg(not(target_os="android"))]
+    {
+        let mut dir = appdirs::user_cache_dir(Some("activitywatch"), None)?;
+        dir.push("aw-server-rust");
+        return Ok(dir);
+    }
+
+    #[cfg(target_os="android")]
+    {
+        return Err(());
+    }
 }

31646a5d8b5b8a0f709868865a9ef6533b0b8d17
Erik Bjäreholt <erik@bjareho.lt>
2018-12-25 16:54:03 +0100

    modified build script to build for different targets

--- compile-android.sh
+++ compile-android.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -x
+set -e
 
-export PATH="$PATH:/home/erb/Programming/activitywatch/other/aw-android/NDK/arm64/bin"
+# Ring won't build in the below target for whatever reason
+# 'arm armv7-linux-androideabi' \
 
-cargo build --target aarch64-linux-android --release --lib
+for archtargetstr in \
+    'x86 i686-linux-android' \
+    'arm64 aarch64-linux-android' \
+; do
+    arch=$(echo $archtargetstr | cut -d " " -f 1)
+    target=$(echo $archtargetstr | cut -d " " -f 2)
+    env PATH="$PATH:/home/erb/Programming/activitywatch/other/aw-android/NDK/$arch/bin" \
+        cargo build --target $target --release --lib
+done

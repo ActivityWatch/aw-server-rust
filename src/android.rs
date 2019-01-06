eb8a82ad19f025643463dbfacd45d16514442487
Erik Bjäreholt <erik@bjareho.lt>
2019-01-06 21:19:45 +0100

    added heartbeat and getEvents, renamed extern functions

--- src/android.rs
+++ src/android.rs
@@ -26,7 +26,7 @@ pub mod android {
     use self::jni::objects::{JClass, JString};
     use self::jni::sys::{jstring};
     use datastore::Datastore;
-    use models::{Bucket};
+    use models::{Event, Bucket};
 
     fn openDatastore() -> Datastore {
         let db_dir = dirs::db_path().to_str().unwrap().to_string();
@@ -34,7 +34,7 @@ pub mod android {
     }
 
     #[no_mangle]
-    pub unsafe extern fn Java_net_activitywatch_android_RustGreetings_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
         // Our Java companion code might pass-in "world" as a string, hence the name.
         let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
         // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
@@ -54,7 +54,7 @@ pub mod android {
     }
 
     #[no_mangle]
-    pub unsafe extern fn Java_net_activitywatch_android_RustGreetings_setAndroidDataDir(env: JNIEnv, _: JClass, java_dir: JString) -> jstring {
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_setAndroidDataDir(env: JNIEnv, _: JClass, java_dir: JString) -> jstring {
         let dir = jstring_to_string(&env, java_dir);
         //let c_str = CStr::from_ptr(env.get_string(java_dir).expect("invalid pattern string").as_ptr());
         //let dir = c_str.to_str().unwrap();
@@ -68,13 +68,13 @@ pub mod android {
     }
 
     #[no_mangle]
-    pub unsafe extern fn Java_net_activitywatch_android_RustGreetings_getBucketsJSONString(env: JNIEnv, _: JClass) -> jstring {
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_getBuckets(env: JNIEnv, _: JClass) -> jstring {
         let buckets = openDatastore().get_buckets().unwrap();
         string_to_jstring(&env, json!(buckets).to_string())
     }
 
     #[no_mangle]
-    pub unsafe extern fn Java_net_activitywatch_android_RustGreetings_createBucket(env: JNIEnv, _: JClass, java_bucket: JString) -> jstring {
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_createBucket(env: JNIEnv, _: JClass, java_bucket: JString) -> jstring {
         let bucket = jstring_to_string(&env, java_bucket);
         let bucket_json: Bucket = match serde_json::from_str(&bucket) {
             Ok(json) => json,
@@ -83,6 +83,29 @@ pub mod android {
         match openDatastore().create_bucket(&bucket_json) {
             Ok(()) => string_to_jstring(&env, "Bucket successfully created".to_string()),
             Err(_) => string_to_jstring(&env, "Something went wrong when trying to create bucket".to_string())
+        }
+    }
+
+    #[no_mangle]
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_heartbeat(env: JNIEnv, _: JClass, java_bucket_id: JString, java_event: JString) -> jstring {
+        let bucket_id = jstring_to_string(&env, java_bucket_id);
+        let event = jstring_to_string(&env, java_event);
+        let event_json: Event = match serde_json::from_str(&event) {
+            Ok(json) => json,
+            Err(err) => return string_to_jstring(&env, err.to_string())
+        };
+        match openDatastore().heartbeat(&bucket_id, event_json, 60.0) {
+            Ok(()) => string_to_jstring(&env, "Heartbeat successfully received".to_string()),
+            Err(_) => string_to_jstring(&env, "Something went wrong when trying to send heartbeat".to_string())
+        }
+    }
+
+    #[no_mangle]
+    pub unsafe extern fn Java_net_activitywatch_android_RustInterface_getEvents(env: JNIEnv, _: JClass, java_bucket_id: JString) -> jstring {
+        let bucket_id = jstring_to_string(&env, java_bucket_id);
+        match openDatastore().get_events(&bucket_id, None, None, None) {
+            Ok(events) => string_to_jstring(&env, json!(events).to_string()),
+            Err(_) => string_to_jstring(&env, "Something went wrong when trying to send heartbeat".to_string())
         }
     }
 }

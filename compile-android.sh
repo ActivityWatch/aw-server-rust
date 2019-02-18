660783a93a1143e23456e4be569a550fecfc7bf7
Erik Bjäreholt <erik@bjareho.lt>
2019-02-18 20:19:33 +0100

    fixed issues in travis config

--- compile-android.sh
+++ compile-android.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ -z "$ANDROID_NDK_HOME" ]; then
-    echo "Environment variable ANDROID_NDK_HOME not set, please set to location of Android NDK."
+if [ -z "$NDK_HOME" ]; then
+    echo "Environment variable NDK_HOME not set, please set to location of Android NDK."
     exit 1
 fi
 
@@ -15,15 +15,14 @@ ORIG_PATH="$PATH"
 for archtargetstr in \
     'x86 i686-linux-android' \
     'arm64 aarch64-linux-android' \
-    'arm armv7-linux-androideabi' \
 ; do
     arch=$(echo $archtargetstr | cut -d " " -f 1)
     target=$(echo $archtargetstr | cut -d " " -f 2)
-    NDK_DIR="/home/erb/Programming/activitywatch/other/aw-android/NDK/$arch/bin"
+    NDK_ARCH_DIR="$NDK_HOME/$arch/bin"
     echo "Building for $arch..."
 
-    if [ -d "$NDK_DIR" ]; then
-        env PATH="$ORIG_PATH:$NDK_DIR" \
+    if [ -d "$NDK_ARCH_DIR" ]; then
+        env PATH="$ORIG_PATH:$NDK_ARCH_DIR" \
             cargo build --target $target --release --lib
     else
         echo "Couldn't find directory for target $arch"

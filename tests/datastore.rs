6c60736eb9acb00482cab91be6420a41b8af9aa7
Erik Bjäreholt <erik@bjareho.lt>
2019-10-29 11:37:31 +0100

    fixed test on Windows (caused by low-precision clock)

--- tests/datastore.rs
+++ tests/datastore.rs
@@ -74,7 +74,7 @@ mod datastore_tests {
             None => panic!("Expected 'None' in bucket to be replaced with current time"),
             Some(created) => {
                 let now = Utc::now();
-                assert!(created < now);
+                assert!(created <= now);
                 assert!(created > now - Duration::seconds(10));
             }
         };

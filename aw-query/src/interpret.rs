6e5916a666714a3b13254c8e2e0a49985d874c9e
Johan Bjäreholt <johan@bjareho.lt>
2019-12-28 14:46:01 +0100

    aw-query: Split lib file into multiple files

---
+++ aw-query/src/interpret.rs
@@ -0,0 +1,186 @@
+use std::collections::HashMap;
+
+use crate::functions;
+
+use aw_models::TimeInterval;
+use aw_datastore::Datastore;
+
+use crate::ast::*;
+use crate::DataType;
+use crate::QueryError;
+
+fn init_env<'a>(ti: &TimeInterval) -> HashMap<&'a str, DataType> {
+    let mut env = HashMap::new();
+    env.insert("TIMEINTERVAL", DataType::String(ti.to_string()));
+    functions::fill_env(&mut env);
+    return env;
+}
+
+pub fn interpret_prog<'a>(p: &'a Program, ti: &TimeInterval, ds: &Datastore) -> Result<DataType, QueryError> {
+    let mut env = init_env(ti);
+    let mut ret = None;
+    for expr in &p.stmts {
+        ret = Some(interpret_expr(&mut env, ds, expr)?)
+    }
+    match ret {
+        Some(ret) => Ok(ret),
+        None => Err(QueryError::EmptyQuery())
+    }
+}
+
+fn interpret_expr<'a>(env: &mut HashMap<&'a str, DataType>, ds: &Datastore, expr: &'a Expr) -> Result<DataType, QueryError> {
+    use crate::ast::Expr_::*;
+    match expr.node {
+        Add(ref a, ref b) => {
+            let a_res = interpret_expr(env, ds, a)?;
+            let b_res = interpret_expr(env, ds, b)?;
+            let res = match a_res {
+                DataType::Number(n1) => match b_res {
+                    DataType::Number(n2) => DataType::Number(n1+n2),
+                    _ => return Err(QueryError::InvalidType("Cannot use + on something that is not a number with a number!".to_string()))
+                },
+                DataType::List(l1) => match b_res {
+                    DataType::List(l2) => {
+                        let mut new_list = l1.clone();
+                        new_list.append(&mut l2.clone());
+                        DataType::List(new_list)
+                    },
+                    _ => return Err(QueryError::InvalidType("Cannot use + on something that is not a list with a list!".to_string()))
+                }
+                DataType::String(s1) => match b_res {
+                    DataType::String(s2) => {
+                        let mut new_string = s1.clone();
+                        new_string.push_str(&s2);
+                        DataType::String(new_string)
+                    },
+                    _ => return Err(QueryError::InvalidType("Cannot use + on something that is not a list with a list!".to_string()))
+                }
+                _ => return Err(QueryError::InvalidType("Cannot use + on something that is not a number, list or string!".to_string()))
+            };
+            Ok(res)
+        },
+        Sub(ref a, ref b) => {
+            let a_res = interpret_expr(env, ds, a)?;
+            let b_res = interpret_expr(env, ds, b)?;
+            let a_num = match a_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            let b_num = match b_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            Ok(DataType::Number(a_num-b_num))
+        },
+        Mul(ref a, ref b) => {
+            let a_res = interpret_expr(env, ds, a)?;
+            let b_res = interpret_expr(env, ds, b)?;
+            let a_num = match a_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            let b_num = match b_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            Ok(DataType::Number(a_num*b_num))
+        },
+        Div(ref a, ref b) => {
+            let a_res = interpret_expr(env, ds, a)?;
+            let b_res = interpret_expr(env, ds, b)?;
+            let a_num = match a_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            let b_num = match b_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            if b_num == 0.0 {
+                return Err(QueryError::MathError("Tried to divide by zero!".to_string()));
+            }
+            Ok(DataType::Number(a_num/b_num))
+        },
+        Mod(ref a, ref b) => {
+            let a_res = interpret_expr(env, ds, a)?;
+            let b_res = interpret_expr(env, ds, b)?;
+            let a_num = match a_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            let b_num = match b_res {
+                DataType::Number(n) => n,
+                _ => return Err(QueryError::InvalidType("Cannot sub something that is not a number!".to_string()))
+            };
+            Ok(DataType::Number(a_num%b_num))
+        },
+        Equal(ref lhs, ref rhs) => {
+            let lhs_res = interpret_expr(env, ds, lhs)?;
+            let rhs_res = interpret_expr(env, ds, rhs)?;
+            Ok(DataType::Bool(lhs_res.query_eq(&rhs_res)?))
+        },
+        Assign(ref var, ref b) => {
+            let val = interpret_expr(env, ds, b)?;
+            // FIXME: avoid clone, it's slow
+            env.insert(var, val.clone());
+            Ok(val)
+        }
+        // FIXME: avoid clone, it's slow
+        Var(ref var) => {
+            match env.get(&var[..]) {
+                Some(v) => Ok(v.clone()),
+                None => Err(QueryError::VariableNotDefined(var.to_string()))
+            }
+        },
+        Bool(lit) => Ok(DataType::Bool(lit)),
+        Number(lit) => Ok(DataType::Number(lit)),
+        String(ref litstr) => Ok(DataType::String(litstr.to_string())),
+        Return(ref e) => {
+            let val = interpret_expr(env, ds, e)?;
+            Ok(val)
+        },
+        If(ref ifs) => {
+            for (ref cond, ref block) in ifs {
+                let c = interpret_expr(env, ds, cond)?;
+                if c.query_eq(&DataType::Bool(true))? {
+                    for expr in block {
+                        interpret_expr(env, ds, expr)?;
+                    }
+                    break;
+                }
+            }
+            Ok(DataType::None())
+        },
+        Function(ref fname, ref e) => {
+            let args = match interpret_expr(env, ds, e)? {
+                DataType::List(l) => l,
+                _ => unreachable!(),
+            };
+            let var = match env.get(&fname[..]) {
+                Some(v) => v,
+                None => return Err(QueryError::VariableNotDefined(fname.clone()))
+            };
+            let (_name, fun) = match var {
+                DataType::Function(name, fun) => (name, fun),
+                _data => return Err(QueryError::InvalidType(fname.to_string()))
+            };
+            fun(args, env, ds)
+        },
+        List(ref list) => {
+            let mut l = Vec::new();
+            for entry in list {
+                let res = interpret_expr(env, ds, entry)?;
+                l.push(res);
+            }
+            Ok(DataType::List(l))
+        }
+        Dict(ref d) => {
+            let mut dict = HashMap::new();
+            for (key, val_uninterpreted) in d {
+                let val = interpret_expr(env, ds, val_uninterpreted)?;
+                dict.insert(key.clone(), val);
+            }
+            Ok(DataType::Dict(dict))
+        }
+    }
+}
---
+++ aw-query/src/lexer.rs
@@ -0,0 +1,141 @@
+use plex::lexer;
+
+#[derive(Debug, Clone)]
+pub enum Token {
+    Ident(String),
+
+    If,
+    ElseIf,
+    Else,
+    Return,
+
+    Bool(bool),
+    Number(f64),
+    String(String),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    Equals,
+    Assign,
+    LParen,
+    RParen,
+    LBracket,
+    RBracket,
+    LBrace,
+    RBrace,
+    Comma,
+    Colon,
+    Semi,
+
+    Whitespace,
+    Newline,
+    Comment,
+}
+
+lexer! {
+    fn next_token(text: 'a) -> (Token, &'a str);
+
+    r#"[ \t\r]+"# => (Token::Whitespace, text),
+    r#"\n"# => (Token::Newline, text),
+    // Python-style comments (# ...)
+    r#"#[^\n]*"# => (Token::Comment, text),
+
+    r#"if"# => (Token::If, text),
+    r#"elif"# => (Token::ElseIf, text),
+    r#"else"# => (Token::Else, text),
+    r#"return"# => (Token::Return, text),
+
+    r#"true"# => (Token::Bool(true), text),
+    r#"false"# => (Token::Bool(false), text),
+    // TODO: Deprecate/Remove?
+    r#"True"# => (Token::Bool(true), text),
+    r#"False"# => (Token::Bool(false), text),
+
+    r#"\"([^\"]|(\\\"))*\""# => (
+        Token::String(text.to_owned()[1..text.len()-1].replace("\\\"", "\"").to_string()),
+        text
+    ),
+    r#"[0-9]+[\.]?[0-9]*"# => {
+        let tok = match text.parse() {
+            Ok(n) => Token::Number(n),
+            Err(e) => panic!("Integer {} is out of range: {}", text, e),
+        };
+        (tok, text)
+    }
+
+    r#"[a-zA-Z_][a-zA-Z0-9_]*"# => (Token::Ident(text.to_owned()), text),
+
+    r#"=="# => (Token::Equals, text),
+    r#"="# => (Token::Assign, text),
+    r#"\+"# => (Token::Plus, text),
+    r#"-"# => (Token::Minus, text),
+    r#"\*"# => (Token::Star, text),
+    r#"/"# => (Token::Slash, text),
+    r#"%"# => (Token::Percent, text),
+    r#"\("# => (Token::LParen, text),
+    r#"\)"# => (Token::RParen, text),
+    r#"\["# => (Token::LBracket, text),
+    r#"\]"# => (Token::RBracket, text),
+    r#"\{"# => (Token::LBrace, text),
+    r#"\}"# => (Token::RBrace, text),
+    r#","# => (Token::Comma, text),
+    r#":"# => (Token::Colon, text),
+    r#";"# => (Token::Semi, text),
+}
+
+pub struct Lexer<'a> {
+    original: &'a str,
+    remaining: &'a str,
+    line: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(s: &'a str) -> Lexer<'a> {
+        Lexer { original: s, remaining: s , line: 1}
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Span {
+    pub lo: usize,
+    pub hi: usize,
+    pub line: usize,
+}
+
+fn span_in(s: &str, t: &str, l: usize) -> Span {
+    let lo = s.as_ptr() as usize - t.as_ptr() as usize;
+    Span {
+        lo: lo,
+        hi: lo + s.len(),
+        line: l,
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = (Token, Span);
+    fn next(&mut self) -> Option<(Token, Span)> {
+        loop {
+            let tok = if let Some((tok, new_remaining)) = next_token(self.remaining) {
+                self.remaining = new_remaining;
+                tok
+            } else {
+                return None
+            };
+            match tok {
+                (Token::Whitespace, _) | (Token::Comment, _) => {
+                    continue;
+                }
+                (Token::Newline, _) => {
+                    self.line += 1;
+                    continue;
+                }
+                (tok, span) => {
+                    return Some((tok, span_in(span, self.original, self.line)));
+                }
+            }
+        }
+    }
+}
+

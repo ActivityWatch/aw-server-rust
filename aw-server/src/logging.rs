afbb70cea31862263f1f8379385f630a6fd49e27
Johan Bjäreholt <johan@bjareho.lt>
2020-03-28 14:47:46 +0100

    aw-server: Increase default log levels further

--- aw-server/src/logging.rs
+++ aw-server/src/logging.rs
@@ -29,8 +29,9 @@ pub fn setup_logger(testing: bool) -> Result<(), fern::InitError> {
         // Set some Rocket messages to debug level
         // TODO: Log more if run in development/testing mode
         .level(log::LevelFilter::Info)
-        .level_for("rocket::rocket", log::LevelFilter::Warn)
-        .level_for("_", log::LevelFilter::Warn)
+        .level_for("rocket", log::LevelFilter::Warn)
+        .level_for("_", log::LevelFilter::Warn) // Rocket requests
+        .level_for("launch_", log::LevelFilter::Warn) // Rocket config info
         .format(move |out, message, record| {
             out.finish(format_args!(
                 "[{}][{}][{}]: {}",
@@ -43,13 +44,11 @@ pub fn setup_logger(testing: bool) -> Result<(), fern::InitError> {
         // Color and higher log levels to stdout
         .chain(
             fern::Dispatch::new()
-                //.level_for("aw_server", log::LevelFilter::Debug)
                 .chain(std::io::stdout()),
         )
         // No color and lower log levels to logfile
         .chain(
             fern::Dispatch::new()
-                .level_for("aw_server", log::LevelFilter::Info)
                 .format(|out, message, _record| {
                     out.finish(format_args!(
                         // TODO: Strip color info

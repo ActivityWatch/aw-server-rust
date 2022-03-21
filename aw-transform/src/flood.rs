682fafc2a48387ae5a06c6099e7fb8ca21add815
Erik Bjäreholt <erik@bjareho.lt>
2022-03-21 12:11:33 +0100

    fix: further improved flooding logic and tests

--- aw-transform/src/flood.rs
+++ aw-transform/src/flood.rs
@@ -2,9 +2,14 @@ use aw_models::Event;
 
 use crate::sort_by_timestamp;
 
-/// Floods event to the nearest neighbouring event if within the specified pulsetime
+/// Floods event to the nearest neighbouring event if within the specified pulsetime.
 ///
-/// Also merges events if they have the same data and are within the pulsetime
+/// Also merges events if they have the same data and are within the pulsetime.
+///
+/// "Flooding" actually performs two similar, but slightly different tasks, which we can describe as:
+///  - Flooding: removes small gaps between events, by extending nearby events to fill the space.
+///  - Merging: merging adjacent or overlapping events that have the same data.
+///     - This is sometimes done in conjunction with flooding, if events with nearby events have the same data.
 ///
 /// Python implementation:
 ///     https://github.com/ActivityWatch/aw-core/blob/master/aw_transform/flood.py
@@ -62,68 +67,78 @@ pub fn flood(events: Vec<Event>, pulsetime: chrono::Duration) -> Vec<Event> {
             }
         };
 
+        // Ensure that ordering is preserved, at least in tests
+        debug_assert!(e1.timestamp <= e2.timestamp);
+
         let gap = e2.timestamp - e1.calculate_endtime();
 
-        // Python implementation:
-        //
-        // if gap < timedelta(0) and e1.data == e2.data:
-        //     start = min(e1.timestamp, e2.timestamp)
-        //     end = max(e1.timestamp + e1.duration, e2.timestamp + e2.duration)
-        //     e1.timestamp, e1.duration = start, (end - start)
-        //     e2.timestamp, e2.duration = end, timedelta(0)
-        //     if not warned_about_negative_gap_safe:
-        //         logger.warning(
-        //             "Gap was of negative duration but could be safely merged ({}s). This message will only show once per batch.".format(
-        //                 gap.total_seconds()
-        //             )
-        //         )
-        //         warned_about_negative_gap_safe = True
+        // We split the program flow into 2 parts: positive and negative gaps
+        // First we check negative gaps (if events overlap)
+        if gap < chrono::Duration::seconds(0) {
+            // Python implementation:
+            //
+            // if gap < timedelta(0) and e1.data == e2.data:
+            //     start = min(e1.timestamp, e2.timestamp)
+            //     end = max(e1.timestamp + e1.duration, e2.timestamp + e2.duration)
+            //     e1.timestamp, e1.duration = start, (end - start)
+            //     e2.timestamp, e2.duration = end, timedelta(0)
+            //     if not warned_about_negative_gap_safe:
+            //         logger.warning(
+            //             "Gap was of negative duration but could be safely merged ({}s). This message will only show once per batch.".format(
+            //                 gap.total_seconds()
+            //             )
+            //         )
+            //         warned_about_negative_gap_safe = True
 
-        // Sanity check in case events overlap
-        if gap < chrono::Duration::seconds(0) && e1.data == e2.data {
-            // Gap was negative and could be safely merged
-            if !warned_negative_gap_safe {
-                warn!("Gap was of negative duration ({}s), but could be safely merged. This error will only show once per batch.", gap);
-                warned_negative_gap_safe = true;
+            // If data is same, we can safely merge them.
+            if e1.data == e2.data {
+                // Gap was negative and could be safely merged
+                if !warned_negative_gap_safe {
+                    warn!("Gap was of negative duration ({}s), but could be safely merged. This error will only show once per batch.", gap);
+                    warned_negative_gap_safe = true;
+                }
+                let start = std::cmp::min(e1.timestamp, e2.timestamp);
+                let end = std::cmp::max(e1.calculate_endtime(), e2.calculate_endtime()); // e2 isn't guarranteed to end last
+                e1.timestamp = start;
+                e1.duration = end - start;
+                // Drop next event since it is merged/flooded into e1
+                e1_iter.next();
+                // Retry this event again to give it a change to merge e1
+                // with 'e3'
+                retry_e = Some(e1);
+                continue;
+            // If data differs and gap is negative beyond the trim thres, throw a warning
+            } else if gap < -negative_gap_trim_thres && !warned_negative_gap_unsafe {
+                // Events with negative gap but differing data cannot be merged safely
+                warn!("Gap was of negative duration and could NOT be safely merged ({}s). This warning will only show once per batch.", gap);
+                warned_negative_gap_unsafe = true;
             }
-            let start = std::cmp::min(e1.timestamp, e2.timestamp);
-            let end = std::cmp::max(e1.timestamp + e1.duration, e2.timestamp + e2.duration);
-            e1.timestamp = start;
-            e1.duration = end - start;
-            // Drop next event since they are merged and flooded into e1
-            e1_iter.next();
-            // Retry this event again to give it a change to merge e1
-            // with 'e3'
-            retry_e = Some(e1);
-            continue;
-        }
-        // Python implementation:
-        //
-        // elif gap < -negative_gap_trim_thres and not warned_about_negative_gap_unsafe:
-        //     # Events with negative gap but differing data cannot be merged safely
-        //     logger.warning(
-        //         "Gap was of negative duration and could NOT be safely merged ({}s). This warning will only show once per batch.".format(
-        //             gap.total_seconds()
-        //         )
-        //     )
-        //     warned_about_negative_gap_unsafe = True
 
-        // If gap is negative beyond the trim thres, throw a warning
-        else if gap < -negative_gap_trim_thres && !warned_negative_gap_unsafe {
-            // Events with negative gap but differing data cannot be merged safely
-            warn!("Gap was of negative duration and could NOT be safely merged ({}s). This warning will only show once per batch.", gap);
-            warned_negative_gap_unsafe = true;
+        // By now, we've ensured gap is non-negative, now we want to know if the gap is smaller
+        // than pulsetime, in which case we should fill it.
         } else if gap < pulsetime {
+            // Python implementation:
+            //
+            // elif gap < -negative_gap_trim_thres and not warned_about_negative_gap_unsafe:
+            //     # Events with negative gap but differing data cannot be merged safely
+            //     logger.warning(
+            //         "Gap was of negative duration and could NOT be safely merged ({}s). This warning will only show once per batch.".format(
+            //             gap.total_seconds()
+            //         )
+            //     )
+            //     warned_about_negative_gap_unsafe = True
+
+            // Ensure that gap is actually non-negative here, at least in tests
+            debug_assert!(gap >= chrono::Duration::seconds(0));
+
+            // If data is the same, we should merge them.
             if e1.data == e2.data {
                 // Choose the longest event and set the endtime to it
-                let e1_endtime = e1.calculate_endtime();
-                let e2_endtime = e2.calculate_endtime();
-                if e2_endtime > e1_endtime {
-                    e1.duration = e2_endtime - e1.timestamp;
-                } else {
-                    e1.duration = e1_endtime - e1.timestamp;
-                }
-                // Drop next event since they are merged and flooded into e1
+                let start = std::cmp::min(e1.timestamp, e2.timestamp); // isn't e1 guarranteed to start?
+                let end = std::cmp::max(e1.calculate_endtime(), e2.calculate_endtime()); // e2 isn't guarranteed to end last
+                e1.timestamp = start;
+                e1.duration = end - start;
+                // Drop next event since it is merged/flooded into e1
                 e1_iter.next();
                 // Retry this event again to give it a change to merge e1
                 // with 'e3'
@@ -132,19 +147,15 @@ pub fn flood(events: Vec<Event>, pulsetime: chrono::Duration) -> Vec<Event> {
                 // to the new_events vec
                 continue;
             } else {
-                if gap < chrono::Duration::seconds(0) {
-                    if !warned_negative_gap_unsafe {
-                        warn!("Gap was of negative duration ({}s) and could NOT be safely merged. This error will only show once per batch.", gap);
-                        warned_negative_gap_unsafe = true;
-                    }
-                } else {
-                    // Extend e1 to the middle between e1 and e2
-                    e1.duration = e1.duration + (gap / 2);
-                }
-                // Make sure next event is pre-extended
+                // Extend e1 to middle of the gap.
+                e1.duration = e1.duration + (gap / 2);
+
+                // Make sure next event (e2) is gets extended before it's processed
                 gap_prev = Some(gap);
             }
         }
+        // else: nothing to do, events not near each other
+
         new_events.push(e1);
     }
     new_events
@@ -163,7 +174,7 @@ mod tests {
     use super::flood;
 
     #[test]
-    fn test_flood() {
+    fn test_flood_merge() {
         // Test merging of events with the same data
         let e1 = Event {
             id: None,
@@ -186,7 +197,10 @@ mod tests {
         let res = flood(vec![e1.clone(), e2.clone()], Duration::seconds(5));
         assert_eq!(1, res.len());
         assert_eq!(&res[0], &e_expected);
+    }
 
+    #[test]
+    fn test_flood_meet_in_middle() {
         // Test flood gap between two different events which should meet in the middle
         let e1 = Event {
             id: None,
@@ -220,7 +234,7 @@ mod tests {
 
     #[test]
     fn test_flood_containing() {
-        // tests flooding an identical event contained within another event
+        // Tests flooding an identical event contained within another event
         let e1 = Event {
             id: None,
             timestamp: DateTime::from_str("2000-01-01T00:00:00Z").unwrap(),

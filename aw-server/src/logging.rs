115cdfbefa5c8688e2cc1651d30f32c9acd2bec0
Harsha Raghu <narnindi.raghu@gmail.com>
2022-10-28 02:37:18 +0530

    fix conflicts

--- aw-server/src/logging.rs
+++ aw-server/src/logging.rs
@@ -25,73 +25,84 @@ pub fn setup_logger(testing: bool) -> Result<(), fern::InitError> {
         .warn(Color::Yellow)
         .error(Color::Red);
 
-    match testing {
-        true => fern::Dispatch::new()
-                // Set some Rocket messages to debug level
-                // TODO: Log more if run in development/testing mode
-                .level(log::LevelFilter::Debug)
-                .level_for("rocket", log::LevelFilter::Debug)
-                .level_for("_", log::LevelFilter::Debug) // Rocket requests
-                .level_for("launch_", log::LevelFilter::Debug) // Rocket config info
-                .format(move |out, message, record| {
-                    out.finish(format_args!(
-                        "[{}][{}][{}]: {}",
-                        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                        colors.color(record.level()),
-                        record.target(),
-                        message,
-                    ))
-                })
-                // Color and higher log levels to stdout
-                .chain(fern::Dispatch::new().chain(std::io::stdout()))
-                // No color and lower log levels to logfile
-                .chain(
-                    fern::Dispatch::new()
-                        .format(|out, message, _record| {
-                            out.finish(format_args!(
-                                // TODO: Strip color info
-                                "{}",
-                                message,
-                            ))
-                        })
-                        .chain(fern::log_file(logfile_path)?),
-                )
-                .apply()?,
+    let default_log_level = if testing {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+
+    let log_level = match std::env::var("LOG_LEVEL")
+        .unwrap_or("info".to_string())
+        .to_lowercase()
+        .as_str()
+    {
+        "trace" => log::LevelFilter::Trace,
+        "debug" => log::LevelFilter::Debug,
+        "info" => log::LevelFilter::Info,
+        "warn" => log::LevelFilter::Warn,
+        "error" => log::LevelFilter::Error,
+        _ => default_log_level,
+    };
+
+    match log_level {
+        log::LevelFilter::Debug | log::LevelFilter::Trace => fern::Dispatch::new()
+            // Set some Rocket messages to debug level
+            .level(log_level)
+            .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "[{}][{}][{}]: {}",
+                    chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    colors.color(record.level()),
+                    record.target(),
+                    message,
+                ))
+            })
+            // Color and higher log levels to stdout
+            .chain(fern::Dispatch::new().chain(std::io::stdout()))
+            // No color and lower log levels to logfile
+            .chain(
+                fern::Dispatch::new()
+                    .format(|out, message, _record| {
+                        out.finish(format_args!(
+                            // TODO: Strip color info
+                            "{}",
+                            message,
+                        ))
+                    })
+                    .chain(fern::log_file(logfile_path)?),
+            )
+            .apply()?,
         _ => fern::Dispatch::new()
-                // Set some Rocket messages to debug level
-                // TODO: Log more if run in development/testing mode
-                .level(log::LevelFilter::Info)
-                .level_for("rocket", log::LevelFilter::Warn)
-                .level_for("_", log::LevelFilter::Warn) // Rocket requests
-                .level_for("launch_", log::LevelFilter::Warn) // Rocket config info
-                .format(move |out, message, record| {
-                    out.finish(format_args!(
-                        "[{}][{}][{}]: {}",
-                        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                        colors.color(record.level()),
-                        record.target(),
-                        message,
-                    ))
-                })
-                // Color and higher log levels to stdout
-                .chain(fern::Dispatch::new().chain(std::io::stdout()))
-                // No color and lower log levels to logfile
-                .chain(
-                    fern::Dispatch::new()
-                        .format(|out, message, _record| {
-                            out.finish(format_args!(
-                                // TODO: Strip color info
-                                "{}",
-                                message,
-                            ))
-                        })
-                        .chain(fern::log_file(logfile_path)?),
-                )
-                .apply()?,
+            // Set some Rocket messages to debug level
+            .level(log_level)
+            .level_for("rocket", log::LevelFilter::Warn)
+            .level_for("_", log::LevelFilter::Warn) // Rocket requests
+            .level_for("launch_", log::LevelFilter::Warn) // Rocket config info
+            .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "[{}][{}][{}]: {}",
+                    chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    colors.color(record.level()),
+                    record.target(),
+                    message,
+                ))
+            })
+            // Color and higher log levels to stdout
+            .chain(fern::Dispatch::new().chain(std::io::stdout()))
+            // No color and lower log levels to logfile
+            .chain(
+                fern::Dispatch::new()
+                    .format(|out, message, _record| {
+                        out.finish(format_args!(
+                            // TODO: Strip color info
+                            "{}",
+                            message,
+                        ))
+                    })
+                    .chain(fern::log_file(logfile_path)?),
+            )
+            .apply()?,
     }
-
-    
-
     Ok(())
 }
 

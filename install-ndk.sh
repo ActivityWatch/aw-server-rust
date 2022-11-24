9bfbb14290c2773e7815c989d41b5f8016d3d15a
Erik Bjäreholt <erik@bjareho.lt>
2022-11-24 14:04:43 +0100

    build: downgrade NDK to r21e and added macOS Android build support

--- install-ndk.sh
+++ install-ndk.sh
@@ -4,15 +4,20 @@
 
 set -e;
 
+NDK_VERSION=r21e
+
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 project_path="$(readlink -f "$script_dir/.")"
+
+platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 if [ -z "$ANDROID_NDK_HOME" ]; then
     if [ -d `pwd`/"NDK" ]; then
         echo "Found NDK folder in root, using."
     else
         echo 'ANDROID_NDK_HOME not set, downloading NDK...';
-        wget --no-verbose -O android-ndk.zip https://dl.google.com/android/repository/android-ndk-r25b-linux.zip;
+        # Download Linux NDK or macOS NDK, depending on OS
+        wget --no-verbose -O android-ndk.zip https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-$platform-x86_64.zip;
         unzip -q -d NDK android-ndk.zip;
         ls NDK;
         mv NDK/*/* NDK/;
@@ -22,7 +27,7 @@ fi
 
 # Needed since dependency 'ring' doesn't respect .cargo/config
 echo "Setting up toolchain binary symlinks..."
-NDK_TOOLCHAIN_BIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
+NDK_TOOLCHAIN_BIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin
 for arch in \
     'aarch64' \
     'x86_64' \
@@ -44,18 +49,18 @@ echo "Creating cargo config..."
 mkdir -p $project_path/.cargo
 echo "
 [target.aarch64-linux-android]
-ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
-linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
+ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/llvm-ar'
+linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/aarch64-linux-android26-clang'
 
 [target.armv7-linux-androideabi]
-ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
-linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi-clang'
+ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/llvm-ar'
+linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/armv7a-linux-androideabi-clang'
 
 [target.i686-linux-android]
-ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
-linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android26-clang'
+ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/llvm-ar'
+linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/i686-linux-android26-clang'
 
 [target.x86_64-linux-android]
-ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
-linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android26-clang'
+ar = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/llvm-ar'
+linker = '$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin/x86_64-linux-android26-clang'
 " > $project_path/.cargo/config

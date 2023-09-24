cb30a4b85219734a6d2f85e0a91545b1500807ae
Erik Bjäreholt <erik@bjareho.lt>
2023-09-24 18:39:17 +0200

    build(android): fix android building

--- compile-android.sh
+++ compile-android.sh
@@ -13,6 +13,7 @@ if [ -z "$ANDROID_NDK_HOME" ]; then
         exit 1
     fi
 fi
+export ANDROID_NDK_HOME
 
 if [ "$RELEASE" = "true" ]; then
     echo "Building in release mode... (slow)";
@@ -22,6 +23,10 @@ else
 fi
 
 ORIG_PATH="$PATH"
+
+# Workaround for "error: unable to find library -lgcc"
+# See: https://stackoverflow.com/questions/68873570/how-do-i-fix-ld-error-unable-to-find-library-lgcc-when-cross-compiling-rust
+find "${ANDROID_NDK_HOME}" -name "libunwind.a" -execdir bash -c 'echo "INPUT(-lunwind)" > libgcc.a' \;
 
 for archtargetstr in \
     'arm64 aarch64-linux-android' \
@@ -33,6 +38,10 @@ for archtargetstr in \
     target=$(echo $archtargetstr | cut -d " " -f 2)
     target_underscore=$(echo $target | sed 's/-/_/g')
 
+
+    echo ARCH $arch
+    echo TARGET $target
+
     NDK_ARCH_DIR="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$platform-x86_64/bin"
     echo "Building for $arch..."
 
@@ -41,6 +50,24 @@ for archtargetstr in \
         # Need to set AR for target since NDK 21+:
         #   https://github.com/rust-lang/cc-rs/issues/636#issuecomment-1075352495
         declare -x "AR_${target_underscore}"="$NDK_ARCH_DIR/llvm-ar"
+        declare -x "CC_${target_underscore}"="$NDK_ARCH_DIR/${target}-clang"
+        declare -x "RANLIB_${target_underscore}"="$NDK_ARCH_DIR/llvm-ranlib"
+
+        # fix armv7 -> arm
+        if [ "$arch" = "arm" ]; then
+            declare -x "CC_${target_underscore}"="$NDK_ARCH_DIR/arm-linux-androideabi-clang"
+        fi
+
+        # check that they exist
+        for var in AR_${target_underscore} CC_${target_underscore} RANLIB_${target_underscore}; do
+            if [ ! -f "${!var}" ]; then
+                echo "Couldn't find ${!var} set for variable $var"
+                exit 1
+            fi
+        done
+
+        # People suggest to use this, but ime it needs all the same workarounds anyway :shrug:
+        #cargo ndk build -p aw-server --target $target --lib $($RELEASE && echo '--release')
         cargo build -p aw-server --target $target --lib $($RELEASE && echo '--release')
     else
         echo "Couldn't find directory $NDK_ARCH_DIR"

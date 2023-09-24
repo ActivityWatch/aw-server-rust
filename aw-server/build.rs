5cb63a5d2133a826152e10fee2e8568696408491
Erik Bjäreholt <erik@bjareho.lt>
2023-09-24 19:10:14 +0200

    build: added build warning if webui dist folder not present

--- aw-server/build.rs
+++ aw-server/build.rs
@@ -1,7 +1,14 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    std::fs::create_dir_all("../aw-webui/dist").unwrap();
+    // if aw-webui/dist does not exist or is empty, print a warning
+    let path = std::path::Path::new("../aw-webui/dist");
+    if !path.exists() || path.read_dir()?.next().is_none() {
+        println!("cargo:warning=`./aw-webui/dist` is not built, compiling without webui");
+    }
+
+    // ensure folder exists, since macro requires it
+    std::fs::create_dir_all(path)?;
     println!("cargo:rustc-env=AW_WEBUI_DIR=../aw-webui/dist");
 
     Ok(())

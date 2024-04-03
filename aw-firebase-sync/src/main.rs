b9eae079b92375997c3f31cde72524177d4b834d
brayo <vukubrian@gmail.com>
2024-04-03 12:31:26 +0300

    working code for testing

--- aw-firebase-sync/src/main.rs
+++ aw-firebase-sync/src/main.rs
@@ -1,31 +1,42 @@
-use tokio;
+// use aw_models::{Query, TimeInterval};
+use reqwest;
+use chrono::{DateTime, TimeZone, Utc};
+use serde_json::{json, Value};
 use aw_client_rust::AwClient;
-use chrono::prelude::*;
-pub struct TimeInterval {
-    pub start: DateTime<Utc>,
-    pub end: DateTime<Utc>,
-}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a new client
-    let client = AwClient::new("localhost", 5600, "my-client").unwrap();
+    let aw_client = AwClient::new("localhost", 5600, "test").unwrap();
 
-    // Define the query
-    let _query = vec![
-        "events = query_bucket(\"aw-watcher-window_my-hostname\");".to_string(),
-        "events = merge_events_by_keys(events, [\"app\"]);".to_string(),
-        "RETURN = sort_by_duration(events);".to_string(),
-    ];
+    let start = Utc.with_ymd_and_hms(2024, 3, 1, 0, 0, 0).unwrap();
+    let end = Utc.with_ymd_and_hms(2024, 4, 1, 0, 0, 0).unwrap();
+    
+    let res = aw_client.get_events("aw-watcher-window_brayo",Some(start),Some(end), Some(50)).await.unwrap();
 
-    let start = Utc.with_ymd_and_hms(2024, 3, 20, 0, 0, 0).unwrap();
-    let end = Utc.with_ymd_and_hms(2024, 3, 28, 0, 0, 0).unwrap();
-    let _timeperiod = TimeInterval{start, end};
+    let res_string = serde_json::to_string(&res).unwrap();
+    // println!("{:?}", res_string);
+    // Your Firebase callable function URL
+    let url = "https://us-central1-aw-mockup.cloudfunctions.net/storeDataREST";
 
-    // let buckets = client.get_buckets().await?;
-    // println!("{:?}", buckets);
-    let data = client.get_events("aw-watcher-window_brayo", Some(start), Some(end), Some(50)).await.unwrap();
-    // Print the result
-    println!("{:?}", data);
+    // Prepare the request body
+    let payload = json!({
+        "apiKey": "fv3yShDm3VHuMjts1P7A+LcjvR66",
+        "data": res_string
+    });
+
+    // Send the POST request
+    let http_client = reqwest::Client::new();
+    let response = http_client
+        .post(url)
+        .json(&payload)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+
+    // Handle the response
+    println!("Response: {:?}", response);
 
     Ok(())
 }

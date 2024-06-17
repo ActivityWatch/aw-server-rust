f15fe71cab460e2f1ffccc876fc2a100f67ec562
brayo <vukubrian@gmail.com>
2024-06-17 16:21:56 +0300

    feat: move config file to platform specific configuration folder Improve error handling

--- aw-firebase-sync/src/main.rs
+++ aw-firebase-sync/src/main.rs
@@ -1,9 +1,10 @@
-use reqwest;
+use aw_client_rust::AwClient;
 use chrono::Utc;
+use dirs::config_dir;
+use reqwest;
 use serde_json::{json, Value};
 use serde_yaml;
-use aw_client_rust::AwClient;
-use std::fs::File;
+use std::fs::{DirBuilder, File};
 use std::io::prelude::*;
 
 #[tokio::main]
@@ -12,11 +13,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Utc::now().date().and_hms_opt(0, 0, 0).unwrap() - chrono::Duration::days(6);
     let end = Utc::now().date().and_hms_opt(0, 0, 0).unwrap() + chrono::Duration::days(1);
 
-    let mut file = File::open("config.yaml").expect("Unable to open file");
+    let path = config_dir()
+        .map(|mut path| {
+            path.push("activitywatch");
+            path.push("aw-firebase-sync");
+            path.push("config.yaml");
+            path
+        })
+        .unwrap();
+
+    if !path.exists() {
+        DirBuilder::new()
+            .recursive(true)
+            .create(path.as_path().parent().expect("Unable to get parent path"))
+            .expect("Unable to create config directory");
+        let mut file = File::create(&path).expect("Unable to create file");
+        file.write_all(b"apikey: your-api-key\n")
+            .expect("Unable to write to file");
+        panic!("Please set your API key at {:?}", path.to_str().unwrap());
+    }
+
+    let mut file = File::open(path).expect("Unable to open file");
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read file");
-    let yaml: Value = serde_yaml::from_str(&contents).unwrap();
-    let apikey = yaml["apikey"].as_str().unwrap().to_string();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+    let yaml: Value = serde_yaml::from_str(&contents).expect("Failed parsing yaml from config file");
+    let apikey = yaml["apikey"].as_str().expect("unable to get api key from config file");
     if apikey == "your-api-key" || apikey == "" {
         panic!("Please set your API key in the config.yaml file");
     }
@@ -24,15 +46,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let query = "window_events = query_bucket(find_bucket(\"aw-watcher-window_\"));
     RETURN = window_events;";
 
-    let timeperiods = vec!(
-        (start, end)
-    );
+    let timeperiods = vec![(start, end)];
 
-    let res = aw_client.query(&query, timeperiods).await.unwrap();
+    // TODO: handle errors
+    let res = aw_client.query(&query, timeperiods).await.unwrap(); 
 
     let res_string = serde_json::to_string(&res).unwrap();
     // strip the leading and trailing '[' and ']'
-    let res_string = &res_string[1..res_string.len()-1];
+    let res_string = &res_string[1..res_string.len() - 1];
 
     let firebase_url = "https://us-central1-aw-mockup.cloudfunctions.net/uploadData";
     // let firebase_url = "http://localhost:5001/aw-mockup/us-central1/uploadData";
